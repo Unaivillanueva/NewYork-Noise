ca453c69f38b1e5f12f0fa518231cb1250286a0a
Julien Hoareau <jhoareau@ec-lyon.fr>
2017-05-07 17:19:29 +0200

    All 4 graphs

--- final-website/data/explanatory_graphs.js
+++ final-website/data/explanatory_graphs.js
@@ -9,64 +9,81 @@ const svg_types = d3.select('#v_types_complaints').append('svg')
                     .style("background-color","#F1F3F3")
 const g_types = svg_types.append('g').attr("transform", `translate(${margin_graphs.left}, ${margin_graphs.top})`);
 
-const x_graphs = d3.scaleLinear().range([0, w_graphs - margin_graphs.left - margin_graphs.right]);
-const y_graphs = d3.scaleBand().range([h_graphs - margin_graphs.top - margin_graphs.bottom, 0]);
+const svg_zones = d3.select('#v_zones_complaints').append('svg')
+                    .attr("width", w_graphs )
+                    .attr("height", h_graphs )
+                    .style("margin", "15px")
+                    .style("background-color","#F1F3F3")
+const g_zones = svg_zones.append('g').attr("transform", `translate(${margin_graphs.left}, ${margin_graphs.top})`);
 
-var tooltip = d3.select('#v_types_complaints').append("div").attr("class","toolTip");
+const svg_time = d3.select('#v_time_complaints').append('svg')
+                    .attr("width", w_graphs )
+                    .attr("height", h_graphs )
+                    .style("margin", "15px")
+                    .style("background-color","#F1F3F3")
+const g_time = svg_time.append('g').attr("transform", `translate(${margin_graphs.left}, ${margin_graphs.top})`);
 
-
-
-d3.csv('data/noise_types.csv', type_data => {
-    type_data = type_data.map(e => ({type: e.type, number: parseInt(e.number)}));
-    type_data.sort((a, b) =>  a.number - b.number);
-    console.log(type_data);
-    x_graphs.domain([0, d3.max(type_data, d => d.number)]);
-    y_graphs.domain(type_data.map(d => d.type)).padding(0.1);
-
-
-    g_types.append("g")
-        .attr("class", "x axis")
-       	.attr("transform", `translate(0, ${h_graphs - margin_graphs.top - margin_graphs.bottom})`)
-      	.call(d3.axisBottom(x_graphs).ticks(5).tickFormat(function(d) { return parseInt(d);}).tickSizeInner([-(h_graphs)]))
-        ;
-    
-    g_types.append("g")
-        .attr("class", "y axis")
-        .call(d3.axisLeft(y_graphs));
-
-    g_types.selectAll('rect')
-        .data(type_data)
-        .enter().append('rect')
-        .attr('class', 'bar')
-        .attr('x', 0)
-        .attr('height', y_graphs.bandwidth())
-        .attr('y', d => y_graphs(d.type))
-        .attr('width', d => x_graphs(d.number))
-        .on("mouseover", function(d){
-            d3.select(this)
-                .transition()
-                .duration(250)
-                .style('fill', '#FF3D0D')
-                .attr('height', y_graphs.bandwidth() + 10)
-                .attr('y', d => y_graphs(d.type) - 5)
-
-            tooltip
-                .style("left", d3.event.pageX - 400 + "px")
-                .style("top", d3.event.pageY - 550 + "px")
-                .style("display", "inline-block")
-                .html((d.type) + "<br>" + (d.number) + " complaints");
-        })
-        .on("mouseout", function(d){
-            d3.select(this)
-                .transition()
-                .duration(250)
-                .style('fill', '#ffa02b')
-                .attr('height', y_graphs.bandwidth())
-                .attr('y', d => y_graphs(d.type))
-
-            tooltip.style("display", "none");
-        }); 
+const svg_month = d3.select('#v_month_complaints').append('svg')
+                    .attr("width", w_graphs )
+                    .attr("height", h_graphs )
+                    .style("margin", "15px")
+                    .style("background-color","#F1F3F3")
+const g_month = svg_month.append('g').attr("transform", `translate(${margin_graphs.left}, ${margin_graphs.top})`);
 
 
+const displayGraph = (fileName, attributeAccessor, svgGroup, parentDiv, color, colorHover) => {
+    d3.csv(fileName, data => {
+        let tooltip = d3.select(parentDiv).append("div").attr("class","toolTip");
+        let x_graphs = d3.scaleLinear().range([0, w_graphs - margin_graphs.left - margin_graphs.right]);
+        let y_graphs = d3.scaleBand().range([h_graphs - margin_graphs.top - margin_graphs.bottom, 0]);
+        data = data.map(e => ({type: e[attributeAccessor], number: parseInt(e.number)}));
+        data.sort((a, b) =>  a.number - b.number);
+        x_graphs.domain([0, d3.max(data, d => d.number)]);
+        y_graphs.domain(data.map(d => d.type)).padding(0.1);
 
-})
+        console.log(data);
+
+        svgGroup.append("g")
+            .attr("class", "x axis")
+            .attr("transform", `translate(0, ${h_graphs - margin_graphs.top - margin_graphs.bottom})`)
+            .call(d3.axisBottom(x_graphs).ticks(5).tickFormat(d => parseInt(d)).tickSizeInner([-(h_graphs)]))
+            ;
+        
+        svgGroup.append("g")
+            .attr("class", "y axis")
+            .call(d3.axisLeft(y_graphs));
+
+        svgGroup.selectAll('rect')
+            .data(data)
+            .enter().append('rect')
+            .attr('class', 'bar')
+            .attr('x', 0)
+            .attr('height', y_graphs.bandwidth())
+            .attr('y', d => y_graphs(d.type))
+            .attr('width', d => x_graphs(d.number))
+            .on("mouseover", function(d){
+                d3.select(this)
+                    .transition()
+                    .duration(100)
+                    .style('fill', color)
+
+                tooltip
+                    .style("left", d3.event.pageX - 400 + "px")
+                    .style("top", d3.event.pageY - 550 + "px")
+                    .style("display", "inline-block")
+                    .html((d.type) + "<br>" + (d.number) + " complaints");
+            })
+            .on("mouseout", function(d){
+                d3.select(this)
+                    .transition()
+                    .duration(100)
+                    .style('fill', colorHover)
+
+                tooltip.style("display", "none");
+            }); 
+    })
+}
+displayGraph('data/noise_types.csv', 'type', g_types, '#v_types_complaints', '#FF3D0D', '#ffa02b');
+displayGraph('data/noise_zones.csv', 'zone',  g_zones, '#v_zones_complaints', '#FF3D0D', '#ffa02b');
+displayGraph('data/noise_time.csv', 'time', g_time, '#v_time_complaints', '#FF3D0D', '#ffa02b');
+displayGraph('data/noise_month.csv', 'month', g_month, '#v_month_complaints', '#FF3D0D', '#ffa02b');
